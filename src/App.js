ffea5b9e443578de7f58e517abe0a7dbe5ae0176
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-14 09:13:47 -0400

    add: fetch jobs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
+
 const url = 'https://course-api.netlify.app/api/react-tabs-project';
+
 function App() {
-  return <h2>tabs project setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
+  return <h2>jobs</h2>;
 }
 
 export default App;
