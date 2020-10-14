cc3f2323a67b3bbe482bd7154a929d76dc226381
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-14 09:35:48 -0400

    add: display buttons

--- src/App.js
+++ src/App.js
@@ -32,12 +32,24 @@ function App() {
   return (
     <section className="section">
       <div className="title">
-        <h2>expierence</h2>
+        <h2>experience</h2>
         <div className="underline"></div>
       </div>
       <div className="jobs-center">
         {/* btn container */}
-
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
         {/* job info */}
         <article className="job-info">
           <h3>{title}</h3>
